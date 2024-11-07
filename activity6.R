3f464942c7e18e43992f49ae99256eb93bc1185b
cjclark3 <cj.clark2021@gmail.com>
2024-11-07 15:55:39 +0000

    hw6

--- activity6.R
+++ activity6.R
@@ -13,16 +13,15 @@ ghg$log.age <- log(ghg$age)
 ghg$log.DIP <- log(ghg$DIP+1)
 ghg$log.precip <- log(ghg$precipitation)
 
-unique(ghg$Region)
+#make regions binary to investigate individual impacts
+unique(ghg$Region) #check regions
 
 # binary variable for boreal region
 ghg$BorealV <- ifelse(ghg$Region == "Boreal",1,0)
 # binary variable for tropical region
 ghg$TropicalV <- ifelse(ghg$Region == "Tropical",1,0)
-
-# binary variable for alpine region
+# binary variable for alpine
 ghg$AlpineV <- ifelse(ghg$Alpine == "yes",1,0)
-
 # binary variable for known hydropower
 ghg$HydroV <- ifelse(ghg$hydropower == "yes",1,0)
 
@@ -34,10 +33,10 @@ mod.full <- lm(log.ch4 ~ airTemp+
                  log.precip+ BorealV, data=ghg) #uses the data argument to specify dataframe
 summary(mod.full)
 
+#assumption testing
 res.full <- rstandard(mod.full)
 fit.full <- fitted.values(mod.full)
 
-#assumption testing
 #qq plot and qqline, normality of residuals
 qqnorm(res.full, pch=19, col="grey50")
 qqline(res.full)
@@ -77,4 +76,91 @@ predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
                                 mean.depth=15,log.DIP=3,
                                 log.precip=6, BorealV=0),
            interval="confidence")
+
+#in class prommpt 1
+ETdat <- read.csv("/cloud/project/ETdata.csv")
+unique(ETdat$crop)
+install.packages(c("lubridate", "forecast"))
+library(lubridate)
+library(forecast)
+
+# average fields for each month for almonds
+almond <- ETdat %>% # ET data
+  filter(crop == "Almonds") %>% # only use almond fields
+  group_by(date) %>% # calculate over each date
+  summarise(ET.in = mean(Ensemble.ET, na.rm = T)) # average fields
+
+# visualize the data
+ggplot(almond, aes(x = ymd(date), y = ET.in)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Year",  y= "Monthy Evapotranspiration (in)") +
+  theme_classic()
+
+# almond ET time series
+almond_ts <- ts(almond$ET.in, # data
+                start = c(2016,1), #start year 2016, month 1
+                #first number is unit of time and second is observations within a unit
+                frequency= 12) # frequency of observations in a unit
+
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
+
+# plot autocorrelations over lag period
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+# partial acf, can better represent potential lag periods
+pacf.plot <- pacf(na.omit(almond_ts))
+
+#run autoregressive modelwith more coefficients
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , # data 
+                order = c(1,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model1
+
+# change order
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
+
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("right", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+# forecast data with uncertainty
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  # Plotting original data
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  # Plotting model forecasts
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ # uncertainty interval
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+#Question 1
 
